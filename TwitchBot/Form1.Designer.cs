2c4ae89107807edc536d5ff189735427486a3409
dqxygamer <39678830+dqxygamer@users.noreply.github.com>
2018-05-27 12:52:20 -0700

    twitchbot

---
+++ TwitchBot/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TwitchBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChatBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ChatBox.Location = new System.Drawing.Point(3, -3);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(805, 717);
+            this.ChatBox.TabIndex = 0;
+            this.ChatBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1268, 709);
+            this.Controls.Add(this.ChatBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+   
+        private RichTextBox ChatBox;
+       }
+    }
+
+
+
+
+
+
